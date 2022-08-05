bb20ef570e211167bd67600a094e05e19f06ced5
Manu Ustenko <ustenko.mi@gmail.com>
2022-08-06 00:07:09 +0200

    Remove auto routes loader

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,20 +1,15 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import store from '@/store';
+import router from './router';
 import '@/plugins/ui';
 import '@/plugins/vuePlugins';
 import '@/common/directives/clickOutside';
 
 Vue.config.productionTip = false;
 
-const init = async() => {
-  const module = await import('@/router');
-  const router = await module.default;
-  new Vue({
-    router,
-    store,
-    render: h => h(App)
-  }).$mount('#app');
-};
-
-init();
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
